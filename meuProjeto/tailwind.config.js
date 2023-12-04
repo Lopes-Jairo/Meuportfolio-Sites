f61b6c346314b4484b578298d8bf71cccb99cbca
Git Reserva <73255336+JairoLopes@users.noreply.github.com>
2023-12-04 16:11:28 -0300

    up

---
+++ meuProjeto/tailwind.config.js
@@ -0,0 +1,124 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./success.html"],
+  theme: {
+    extend: {
+
+      //-------------------------------- ANIMAÇÕES -------------------------------------------------
+      keyframes: {
+
+        slideTop: {
+          '0%': { opacity: '0', transform: 'translateY(10rem)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }
+        },
+
+        slideBottom: {
+          '0%': { opacity: '0', transform: 'translateY(-10rem)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }
+        },
+
+        slideLeft: {
+          '0%': { opacity: '0', transform: 'translateX(10rem)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' }
+        },
+
+        slideRight: {
+          '0%': { opacity: '0', transform: 'translateX(-10rem)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' }
+        },
+
+        zoomIn: {
+          '0%': { opacity: '0', transform: 'scale(0)' },
+          '100%': { opacity: '1', transform: 'scale(1)' }
+        },
+
+        floatImage: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-3.5rem)' }
+        }
+
+      },
+
+
+      animation: {
+        slideTop: 'slideTop 1s 0.4s ease forwards',
+        slideBottom: 'slideBottom 1s 0.4s ease forwards',
+        slideLeft: 'slideLeft 1s 0.5s ease forwards',
+        slideRight: 'slideRight 1s 0.5s ease forwards',
+        zoomIn: 'zoomIn 1s 0.2s ease forwards',
+        floatImage: 'floatImage 4s 0.2s ease-in-out infinite'
+      },
+
+      //___________________________________ FIM ANIMAÇÕES ____________________________________________________
+
+
+      transitionDelay: {
+        '1500': '1500ms',
+        '2000': '2000ms',
+        '2400': '2400ms',
+        '2600': '2600ms',
+        '3200': '3200ms',
+        '4000': '4000ms',
+      },
+
+
+      backgroundImage: {
+
+        'cube': 'url(../img/cube.jpg)',
+        'cube2': 'url(../img/cube2.jpg)'
+
+      },
+
+
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif']
+      },
+
+      colors: {
+        /* 'color-text': '#ff6600', */
+        'color-text': '#0099ff',
+        'color-bg': '#141a1a'
+      },
+
+      screens: {
+        // Telas adaptaveis para a NAV
+        'mobnav': { 'max': '768px' }, // telas do nav para mobile
+        'mobnav2': { 'max': '1049px' }, // telas do nav para mobile
+        'mobnav3': { 'min': '1050px' }, // telas do nav para mobile
+
+        'desknav': { 'min': '769px' }, // telas do nav para desktop em diante
+
+
+
+        // telas adaptaveis para BANNER
+        'deskbanner': { 'max': '1200px' }, // telas do nav para desktop em diante
+
+
+        'small': { 'max': '639px' },
+        'mobile': { 'min': '640px', 'max': '767px' },
+        'tablet': { 'min': '768px', 'max': '1280px' },
+        'desktop': { 'min': '1281px', 'max': '1536px' },
+        'large': '1537'
+
+      },
+
+      spacing: {
+
+        '5percent': '5%',
+        '10percent': '10%',
+        '15percent': '15%',
+        '20percent': '20%',
+        '30percent': '30%',
+        '40percent': '40%',
+        '50percent': '50%',
+        '60percent': '60%',
+        '70percent': '70%',
+        '80percent': '80%',
+        '90percent': '90%',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
